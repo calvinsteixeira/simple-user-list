1b59670a08bd8a01e004b74480dd33017c4a1607
Calvin <calvin@MacBook-Air-de-Calvin.local>
2025-02-12 22:58:51 -0300

    Mockando os dados para iniciar o desenvolvimento da interface

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,37 @@
 import React from "react";
+import UserCard from "./components/UserCard";
 
 export default function index() {
-  return <div>Tela home</div>;
+  const user = {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    address: {
+      street: "Kulas Light",
+      suite: "Apt. 556",
+      city: "Gwenborough",
+      zipcode: "92998-3874",
+      geo: {
+        lat: "-37.3159",
+        lng: "81.1496",
+      },
+    },
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org",
+    company: {
+      name: "Romaguera-Crona",
+      catchPhrase: "Multi-layered client-server neural-net",
+      bs: "harness real-time e-markets",
+    },
+  };
+
+  return (
+    <div className="px-6 py-10 w-screen h-screen">
+      <h1 className="text-xl font-extrabold">SimpleUserList</h1>
+      <div className="mt-10">
+        <UserCard user={user} />
+      </div>
+    </div>
+  );
 }
